ada335fcbebb0408dc48785676de4d129f5a7f9e
teyper <teyjhanabartonneal@spelman.edu>
2025-05-05 20:09:34 -0400

    ASSETS

---
+++ Assets/Attrubutions.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attrubutions : MonoBehaviour
+{
+
+
+    /*
+     * All 3D models: sketchfab.com 
+     * background music: Mississippi Fred Dowell- You Gotta Move: Youtube.com
+     * backgorund haunted forest sound from Youtube.com 
+     * Ytmp3.com:  converter
+     * all Sound effects:freesound.com 
+     * textures: texturebox.com 
+     * animations: mixamo.com 
+     * Unity Asset store: stary night skybox, third and first person player controller
+     *
+     */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
